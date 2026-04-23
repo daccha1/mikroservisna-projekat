bd9564c63d6e69f49f89932f0fe6de298136fb8e
dacha <nijedavid@gmail.com>
2026-04-23 18:55:10 +0200

    added changes to EmailService

--- mikroservisnaApp/EmailService/Program.cs
+++ mikroservisnaApp/EmailService/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace EmailService
+﻿using EmailService.Services;
+
+namespace EmailService
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			
+			EmailSenderClient.StartClient();
 		}
 	}
 }
--- mikroservisnaApp/EmailService/Services/EmailSenderClient.cs
+++ mikroservisnaApp/EmailService/Services/EmailSenderClient.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using Resend;
-
+using Microsoft.Extensions.Configuration;
 
 namespace EmailService.Services
 {
 	public class EmailSenderClient
 	{
-		IResend resend;
-		public async Task StartClient()
+		static IResend resend;
+		public static void StartClient()
 		{
+			var config = new ConfigurationBuilder()
+							.AddJsonFile("secrets.json", optional: false, reloadOnChange: true)
+							.Build();
+
+			string? apiKey = config["resendKey"];
+			Console.WriteLine(apiKey);
+			resend = ResendClient.Create(apiKey);
 			
 		}
 	}
